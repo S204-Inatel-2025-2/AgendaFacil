1f267513bcbabfe0d3e486e1337648d24c8e730d
Marcelo Alckmin <marcelo.alckmin22@gmail.com>
2025-10-13 17:55:52 -0300

    feat: controller do servico com funcoes basicas criado

---
+++ backend/src/main/java/com/agendafacil/backend/controller/ServicoController.java
@@ -0,0 +1,55 @@
+package com.agendafacil.backend.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.agendafacil.backend.DTO.ServicoDTO;
+import com.agendafacil.backend.model.Servico;
+import com.agendafacil.backend.service.ServicoService;
+
+@RestController
+@RequestMapping("/servicos")
+public class ServicoController {
+    @Autowired
+    private ServicoService servicoService;
+
+    @PostMapping("/cadastrar")
+    public Servico cadastrar(@RequestBody ServicoDTO servicoDTO){
+        Servico servico = new Servico(servicoDTO.getNome(), servicoDTO.getCategoria(), servicoDTO.getDescricao(), servicoDTO.getDuracao_minutos(), servicoDTO.getPreco());
+        return servicoService.cadastrar(servico);
+    }
+
+    @GetMapping
+    public List<Servico> getAllServicos(){
+        return servicoService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Servico> getServicoById(@PathVariable Long id){
+        return servicoService.findById(id);
+    }
+
+    @GetMapping("/categoria/{categoria}")
+    public List<Servico> getServicoByCategoria(@PathVariable String categoria){
+        return servicoService.findByCategoria(categoria);
+    }
+
+    @GetMapping("/{nome}")
+    public Optional<Servico> getServicoByNome(@PathVariable String nome){
+        return servicoService.findByNome(nome);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable Long id){
+        servicoService.deletar(id);
+    }
+}
