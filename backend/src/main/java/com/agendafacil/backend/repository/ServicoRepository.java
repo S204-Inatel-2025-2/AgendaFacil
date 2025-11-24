0556df11a852178933c7693f326a203772fde273
Marcelo Alckmin <marcelo.alckmin22@gmail.com>
2025-11-24 16:48:13 -0300

    feat: criando a busca de servicos por empresa

--- backend/src/main/java/com/agendafacil/backend/repository/ServicoRepository.java
+++ backend/src/main/java/com/agendafacil/backend/repository/ServicoRepository.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.agendafacil.backend.model.Servico;
+import com.agendafacil.backend.model.Empresa;
 
 public interface ServicoRepository extends JpaRepository<Servico, Long>{
     Optional<Servico> findByNome(String nome);
     List<Servico> findByCategoria(String categoria);
     List<Servico> findByAgendadoFalse();
+    List<Servico> findByEmpresa(Empresa empresa);
 }
