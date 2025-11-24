0728c22b3049cb2b4c06297bcf89e06f7e411e8e
Marcelo Alckmin <marcelo.alckmin22@gmail.com>
2025-11-24 15:57:34 -0300

    feat: adicionando a busca por email no repository das empresas

--- backend/src/main/java/com/agendafacil/backend/repository/EmpresaRepository.java
+++ backend/src/main/java/com/agendafacil/backend/repository/EmpresaRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmpresaRepository extends JpaRepository<Empresa,Long>{
     Optional<Empresa> findByCnpj(String cnpj);
+    Optional<Empresa> findByEmail(String email);
 }
