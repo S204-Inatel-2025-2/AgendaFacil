799d7df433482c36fc3828577cf384272e0d7feb
Beatriz Cardozo <beatriz.a.cardozo28@gmail.com>
2025-11-24 15:44:37 -0300

    Revert "feat: criando o endpoint de login de empresas e a busca por email no repository"
    This reverts commit c65bf008b7610093df1c7435b40b3493b9925128.
    
    git push
    
    :wq

--- backend/src/main/java/com/agendafacil/backend/controller/EmpresaController.java
+++ backend/src/main/java/com/agendafacil/backend/controller/EmpresaController.java
@@ -1,18 +1,13 @@
 package com.agendafacil.backend.controller;
 
 import com.agendafacil.backend.DTO.EmpresaDTO;
-import com.agendafacil.backend.DTO.LoginDTO;
 import com.agendafacil.backend.service.EmpresaService;
-import com.agendafacil.backend.model.Empresa;
 
 import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/empresas")
+@RequestMapping("/api/empresas")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class EmpresaController {
@@ -21,22 +16,7 @@ public class EmpresaController {
 
     @GetMapping("/cnpj/{cnpj}")
     public EmpresaDTO buscarPorCnpj(@PathVariable String cnpj) {
-        return empresaService.consultaCnpj(cnpj);
-    }
-
-    @PostMapping("/cadastrar")
-    public Empresa cadastrarEmpresa(@RequestBody EmpresaDTO empresaDTO) throws Exception{
-        return empresaService.cadastrarEmpresa(empresaDTO);
-    }
-
-    @PostMapping("/login")
-    public Empresa loginEmpresa(@RequestBody LoginDTO loginDTO) throws Exception{
-        return empresaService.loginEmpresa(loginDTO.getEmail(), loginDTO.getSenha());
-    }
-
-    @GetMapping
-    public List<Empresa> getAllEmpresas(){
-        return empresaService.findAll();
+        return empresaService.obterOuCadastrarEmpresaPorCNPJ(cnpj);
     }
 
 }
--- backend/src/main/java/com/agendafacil/backend/repository/EmpresaRepository.java
+++ backend/src/main/java/com/agendafacil/backend/repository/EmpresaRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmpresaRepository extends JpaRepository<Empresa,Long>{
     Optional<Empresa> findByCnpj(String cnpj);
-    Optional<Empresa> findByEmail(String email);
 }
