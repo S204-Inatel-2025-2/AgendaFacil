e4ae9716fb828c7f86853e84297aa54a9bd8992e
Karolina <karol_osilva@outlook.com>
2025-11-24 07:07:51 -0300

    Ajusta testes Cypress e config

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,17 +2,12 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    // URL do frontend rodando no container
     baseUrl: "http://localhost:4200",
-
-    // onde estão seus testes (opcional, mas bom deixar explícito)
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
 
     viewportWidth: 1920,
     viewportHeight: 1080,
-
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 });
